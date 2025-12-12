907b8274ac3e3a651fb14c43f5d23dea1c466a05
DominikKastenRDF <dominik.kasten@rdf.nuernberg.de>
2025-12-12 08:40:34 +0100

    [DK | 12.12.2025 | added guy.py from old Repo]

--- gui.py
+++ gui.py
@@ -1,12 +1,144 @@
 '''
 Discibtion: This moduls represends the developments of the gui
 Creation Date: 11.12.2025
+Laste Change: 12.12.2025 - added gui.py from old Repo
+Change User: DK
 Last Change: 11.12.2025 - creation of the gui.py file  
 Change User: OP
 '''
 
 
+import os
+os.system("cls")
 
+#Figur Stage 1 von 6
+def figur_stage_1():
+        print(" ",chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))	
+        print(chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
 
+#Figur Stage 2 von 6
+def figur_stage_2():
+        print("   ",chr(9744),chr(9744),chr(9744))
+        print("  ",chr(9744),chr(9744),chr(9744),chr(9744))
+        print(" ",chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))	
+        print(chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
 
+#Figur Stage 3 von 6
+def figur_stage_3():
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("   ",chr(9744),chr(9744),chr(9744))
+        print("  ",chr(9744),chr(9744),chr(9744),chr(9744))
+        print(" ",chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))	
+        print(chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
+
+#Figur Stage 4 von 6
+def figur_stage_4():
+        print("     ",chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("   ",chr(9744),chr(9744),chr(9744))
+        print("  ",chr(9744),chr(9744),chr(9744),chr(9744))
+        print(" ",chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))	
+        print(chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
+
+#Figur Stage 5 von 6
+def figur_stage_5():
+        print("     ",chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
+        print("     ",chr(9744),"       ",chr(9744))
+        print("     ",chr(9744),"       ",chr(9744))
+        print("     ",chr(9744),"       ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("     ",chr(9744))
+        print("   ",chr(9744),chr(9744),chr(9744))
+        print("  ",chr(9744),chr(9744),chr(9744),chr(9744))
+        print(" ",chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))	
+        print(chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
+
+#Figur Stage 6 von 6
+def figur_stage_6():
+    print("     ",chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
+    print("     ",chr(9744),"       ",chr(9744))
+    print("     ",chr(9744),"       ",chr(9744))
+    print("     ",chr(9744),"       ",chr(9744))
+    print("     ",chr(9744),"     ",chr(9744),chr(9744),chr(9744))
+    print("     ",chr(9744),"    ",chr(9744),"   ",chr(9744))
+    print("     ",chr(9744),"     ",chr(9744),chr(9744),chr(9744))
+    print("     ",chr(9744),"       ",chr(9744))
+    print("     ",chr(9744),"   ",chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
+    print("     ",chr(9744),"       ",chr(9744))
+    print("     ",chr(9744),"     ",chr(9744),chr(9744),chr(9744))
+    print("     ",chr(9744),"     ",chr(9744)," ",chr(9744))	
+    print("     ",chr(9744))
+    print("   ",chr(9744),chr(9744),chr(9744))
+    print("  ",chr(9744),chr(9744),chr(9744),chr(9744))
+    print(" ",chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))	
+    print(chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
+
+#Text Prints
+def geheimes_wort():
+       print("Errate folgendes Wort:  ")
+
+def nächster_guess():
+       print("Bitte gib den nächsten Buchstaben ein.")
+
+def falscher_guess():
+       print("Du hast falsch geraten.")
+
+def gewonnen():
+        print("Herzlichen Glückwunsch, du hast Gewonnne \U0001F389\U0001F389\U0001F389")
+
+def verloren():
+       print("Du hast verloren \U0001F613\U0001F613\U0001F613")
+
+#Test
+figur_stage_1()
+print()
+figur_stage_2()
+print()
+figur_stage_3()
+print()
+figur_stage_4()
+print()
+figur_stage_5()
+print()
+figur_stage_6()
+print()
+geheimes_wort()
+print()
+nächster_guess()
+print()
+falscher_guess()
+print()
+gewonnen()
+print()
+verloren()
 
