c4e49e50b6b3e153f1eb0b6b8d740f4900e0fbde
DominikKastenRDF <dominik.kasten@rdf.nuernberg.de>
2025-12-12 10:09:13 +0100

    [DK | 12.12.2025 | written nächser_guess funktion, part of spieler_name]

--- gui.py
+++ gui.py
@@ -1,12 +1,13 @@
 '''
 Discibtion: This moduls represends the developments of the gui
 Creation Date: 11.12.2025
-Laste Change: 12.12.2025 - added gui.py from old Repo
+Last Change: 12.12.2025 - wrote nächser_guess funktion, part of spieler_name
+Change User: DK
+Last Change: 12.12.2025 - added gui.py from old Repo
 Change User: DK
 Last Change: 11.12.2025 - creation of the gui.py file  
 Change User: OP
 '''
-
 
 import os
 os.system("cls")
@@ -104,13 +105,33 @@ def figur_stage_6():
     print(chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744),chr(9744))
 
 #Text Prints
+
+def spieler_namen():
+       spielende_spieler = []
+       spieler = input(f"Bitte geben Sie Ihren Namen an:")
+       spielende_spieler.append = spieler
+       
+
 def geheimes_wort():
-       print("Errate folgendes Wort:  ")
+       with open("woerter.txt", encoding="utf-8") as file_of_words:
+              pass
+              
 
 def nächster_guess():
-       print("Bitte gib den nächsten Buchstaben ein.")
+        while True:
+                import re
+                guess = input(f"Bitte geben Sie den nächsten Buchstaben ein:")
+                if re.fullmatch(r"[A-Za-z]+", guess):
+                        print("Ihr geratener Buchstabe / Wort lautet:",guess)
+                        break
 
+                else:
+                        print("Falsche Eingabe, bitte geben Sie einen Buchstaben ein und keine Zahl oder Sonderzeichen")
+                        
+       
 def falscher_guess():
+
+
        print("Du hast falsch geraten.")
 
 def gewonnen():
@@ -120,25 +141,24 @@ def verloren():
        print("Du hast verloren \U0001F613\U0001F613\U0001F613")
 
 #Test
-figur_stage_1()
-print()
-figur_stage_2()
-print()
-figur_stage_3()
-print()
-figur_stage_4()
-print()
-figur_stage_5()
-print()
-figur_stage_6()
-print()
-geheimes_wort()
-print()
+#figur_stage_1()
+#print()
+#figur_stage_2()
+#print()
+#figur_stage_3()
+#print()
+#figur_stage_4()
+#print()
+#figur_stage_5()
+#print()
+#figur_stage_6()
+#print()
+#geheimes_wort()
+#print()
 nächster_guess()
-print()
-falscher_guess()
-print()
-gewonnen()
-print()
-verloren()
-
+#print()
+#falscher_guess()
+#print()
+#gewonnen()
+#print()
+#verloren()
